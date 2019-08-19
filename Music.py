190154013816c3d7603417a0ea45aec7ef2af283
EnZ09 <54250232+EnZ09@users.noreply.github.com>
2019-08-19 12:51:39 +0300

    Update Music.py

--- Music.py
+++ Music.py
@@ -18,7 +18,12 @@ bot = commands.Bot(command_prefix=BOT_PREFIX)
 @bot.event
 async def on_ready():
     print("Вход выполнен: " + bot.user.name + "\n")
-
+    
+@Bot.event
+async def on_message(message):
+    if message.content == 'привет':
+        await message.channel.send(f'{message.author.mention}, Привет куколд!')
+    
 
 @bot.command(pass_context=True, aliases=['j', 'joi'])
 async def вход(ctx):
